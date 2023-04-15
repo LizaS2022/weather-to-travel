f46871ec72e8b6a7a96a22fd2e831de3690e920e
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-04-15 09:36:14 -0400

    user input field returns value in the console

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 
-var CurrentDateEl = $("#current-date");
-var UserCityChoiceEl = $("#user-input");
+// What do I need to grab? I need to grab the search button, the input field
+var searchBtn = $("#user-input");
+var userInput = $("#user-input");
+var userInputEl = userInput.val().toLowerCase().trim();
+console.log(userInputEl);
 
-var formEl = $("#user-form");
-var searchBtnEl = $("#search-btn");
-
-// // dynamic date component for the header
-CurrentDateEl.text(dayjs().format("MM/DD/YYYY"));
-
-// // taking user input and extracting the data for the city weather
-
-
-searchBtnEl.on("click", function(event){
-  event.preventDefault()
-  var cityInputEl = UserCityChoiceEl.val().toLowerCase().trim();
-  var currentWeatherAPIKey = "https://api.openweathermap.org/data/2.5/weather?q="+ cityInputEl +"&appid=db39f54408315ce8f4c7ba5f7fcc11fb";
-  fetch(currentWeatherAPIKey)
-  .then(function (response) {
-    return response.json();
-  })
-  .then (function(data){
-    console.log(data.main.temp);
-    console.log(data.main.humidity);
-    console.log(data.wind.speed);
-  }) 
-    
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-  
