08ef8881e4288de6a10f6b2f59cd4df6b5253555
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-04-17 09:37:12 -0400

    Need to finish the history button

--- index.js
+++ index.js
@@ -2,100 +2,159 @@
 // What do I need to grab? I need to grab the search button, the input field
 var searchBtn = $("#search-btn");
 var currentDate = dayjs().format("MM/DD/YYYY");
-
-
-
-// what do I need to do? I need to extract information from the API weather. its done when
-// I use the API key to access the current weather conditions
+var wraperDivEl = $("#wrapper-div");
 
 searchBtn.on("click", function(event){
   event.preventDefault();
-  WeatherConditionApiCall();
+  WeatherConditionApiCall();  
+  
 });
 
 function WeatherConditionApiCall() {
-  var userInput = $("#user-input").val().toLowerCase().trim();
-  var currentWeatherApiKey = "http://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=db39f54408315ce8f4c7ba5f7fcc11fb&units=imperial&speed=miles/hour";
+  var city = $("#user-input").val().toLowerCase().trim();
+  var currentWeatherApiKey = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=db39f54408315ce8f4c7ba5f7fcc11fb&units=imperial&speed=miles/hour";
   fetch(currentWeatherApiKey).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
+        console.log(data);
         CurrentWeather(data);
     });
   } else {
       alert("404 Error: Could not found");
   }
-});
+  });
 
-var fiveDayWeatherApiKey = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=698b706d240fcf2ea1163a991d280698&units=imperial&speed=miles/hour";
+  var fiveDayWeatherApiKey = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=698b706d240fcf2ea1163a991d280698&units=imperial&speed=miles/hour";
   fetch(fiveDayWeatherApiKey).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
         fiveDayDisplay(data);
-        
     });
   } else {
       alert("404 Error: Could not found");
   }
-});
-
+  });
+  
 }
 
 function CurrentWeather(data){
-  var userInput = $("#user-input").val().toLowerCase().trim();
-  var mainDiv = $("#main-div");
-  mainDiv.text("");
-  var divEl = $("<div>").attr("class", "container col card mb-4 rounded-3 shadow-sm");
-  mainDiv.append(divEl);
-  var innerDivEl = $("<div>").attr("class","card-body");
-  divEl.append(innerDivEl);
-  var titleEl = $("<h4>").attr("class","my-0 fw-normal");
-  titleEl.text(userInput +" " + currentDate);
-  innerDivEl.append(titleEl);
-  var listEl = $("<ul>").attr("class","list-unstyled mt-3 mb-4");
-  innerDivEl.append(listEl);
-  var li1El = $("<li>").text("Temperature: " + data.main.temp);
-  listEl.append(li1El);
-  var li2El = $("<li>").text("Humidity: " + data.main.humidity);
-  listEl.append(li2El);
-  var li3El = $("<li>").text("Wind: " + data.wind.speed);
-  listEl.append(li3El);
+    var city= $("#user-input").val().toLowerCase().trim();
+    var mainDiv = $("#main-div");
+    mainDiv.text("");
+    var divEl = $("<div>").attr("class", "container col card mb-4 rounded-3 shadow-sm");
+    mainDiv.append(divEl);
+    var innerDivEl = $("<div>").attr("class","card-body");
+    divEl.append(innerDivEl);
+    var titleEl = $("<h4>").attr("class","my-0 fw-normal");
+    titleEl.text(city +" " + currentDate);
+    innerDivEl.append(titleEl);
+    var listEl = $("<ul>").attr("class","list-unstyled mt-3 mb-4");
+    innerDivEl.append(listEl);
+    var li1El = $("<li>").text("Temperature: " + data.main.temp +"F");
+    listEl.append(li1El);
+    var li2El = $("<li>").text("Humidity: " + data.main.humidity + "%");
+    listEl.append(li2El);
+    var li3El = $("<li>").text("Wind: " + data.wind.speed + "mph");
+    listEl.append(li3El);
 
+    setWeatherDataToLocalStorage(data)
+   
+    
 }
 
-// Now I need to display a forecast for five days in 5 cards
-// how its done? Its done when I create an api key,fetch, return data, manipulate it and disoplay on the screen
-
 function fiveDayDisplay(data) {
-  var userInput = $("#user-input").val().toLowerCase().trim();
+  var city = $("#user-input").val().toLowerCase().trim();
   var cardsDivEl = $("#cards-div");
   cardsDivEl.text("");
+  saveDatafiveDay = [];
   for (var i=1; i<data.list.length; i+=8) {
     var dateEl = data.list[i].dt_txt.split(" ")[0];
     var tempEl = data.list[i].main.temp;
     var humidityEl = data.list[i].main.humidity;
     var windEl = data.list[i].wind.speed;
+    
     var divEl = $("<div>").attr("class", "container col card mb-4 rounded-3 shadow-sm");
     cardsDivEl.append(divEl);
     var innerDivEl = $("<div>").attr("class","card-body");
     divEl.append(innerDivEl);
     var titleEl = $("<h4>").attr("class","my-0 fw-normal");
-    titleEl.text(userInput + " " + dateEl);
+    titleEl.text(city + " " + dateEl);
     innerDivEl.append(titleEl);
     var listEl = $("<ul>").attr("class","list-unstyled mt-3 mb-4");
     innerDivEl.append(listEl);
-    var li1El = $("<li>").text("Temperature: " + tempEl);
+    var li1El = $("<li>").text("Temperature: " + tempEl +"F");
     listEl.append(li1El);
-    var li2El = $("<li>").text("Humidity: " + humidityEl);
+    var li2El = $("<li>").text("Humidity: " + humidityEl +"%");
     listEl.append(li2El);
-    var li3El = $("<li>").text("Wind: " + windEl);
+    var li3El = $("<li>").text("Wind: " + windEl + "mph");
     listEl.append(li3El);
+
+    var fiveDayObj = {
+      City: city,
+      Date: dateEl,
+      Temperature: tempEl,
+      Humidity: humidityEl,
+      Wind: windEl,
+    }
+    saveDatafiveDay.push(fiveDayObj);
+  }
+  setFiveDaysToStorage(city,saveDatafiveDay);
   
+
+}
+
+
+function setFiveDaysToStorage(city,arrayOfFiveDays) {
+  var cityWeatherDataArray = JSON.parse(localStorage.getItem(city));
+  // console.log(cityWeatherDataArray);
+  // console.log(arrayOfFiveDays)
+  cityWeatherDataArray = cityWeatherDataArray.concat(arrayOfFiveDays); 
+  localStorage.setItem(city,JSON.stringify(cityWeatherDataArray));
+  
+  var sixDaysWeather =(JSON.parse(localStorage.getItem(city)));
+  console.log(sixDaysWeather);
+  displaySixDaysWeather(sixDaysWeather);
+  
+}
+
+
+
+
+function setWeatherDataToLocalStorage(data) {
+  console.log("thi set current date is called")
+    var city = $("#user-input").val().toLowerCase().trim();
+    var CurrentDayweatherObj = [
+      {
+        city:city,
+        Date: currentDate,
+        Temperature: data.main.temp,
+        Humidity: data.main.humidity,
+        Wind: data.wind.speed
+      }]
+    // console.log(CurrentDayweatherObj)
+    var savetoLocalStorage1 = localStorage.setItem(city,JSON.stringify(CurrentDayweatherObj));
+    // console.log(savetoLocalStorage1);
+}
+
+// forloop the sixdaysweather array, for loop , building the same structure but with the new location of the city,wind,humidity(dote notation)
+function displaySixDaysWeather(sixDaysWeather) {
+  
+  var prevBtnLocation = $("#prev-btn-location");
+  for(var i =0; i <sixDaysWeather.length; i++){
+    
+    var btnHistoryCity = $("<button>").text(sixDaysWeather[i].city);
+    prevBtnLocation.append(btnHistoryCity);
+    btnHistoryCity.on("click", function(event){
+      event.preventDefault();
+
+      
+      
+
+      
+      
+
+    })
   }
 }
 
-// what noew? I need to create a list where previous search cities will appear.
-// how: storing the input in the local storage and getting them
-
-
-  
 
