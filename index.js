2a5b95a19a7dd184784d9d4d1880002f5d7eafb9
Elizabeta Sirota <elizabetasirota@gmail.com>
2023-04-18 09:17:50 -0400

    Review code

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 
-// What do I need to grab? I need to grab the search button, the input field
 var searchBtn = $("#search-btn");
 var currentDate = dayjs().format("MM/DD/YYYY");
 var wraperDivEl = $("#wrapper-div");
 var city = $("#user-input").val().toLowerCase().trim();
 
-
+localStorage.clear();
 
 searchBtn.on("click", function(event){
   event.preventDefault();
@@ -36,39 +35,42 @@ function WeatherConditionApiCall() {
       alert("404 Error: Could not found");
   }
   });
-  
 }
 
+
 function CurrentWeather(data){
-    var city= $("#user-input").val().toLowerCase().trim();
+
+    var city = $("#user-input").val().toLowerCase().trim();
     var mainDiv = $("#main-div");
     mainDiv.text("");
     var divEl = $("<div>").attr("class", "container col card mb-4 rounded-3 shadow-sm");
     mainDiv.append(divEl);
     var innerDivEl = $("<div>").attr("class","card-body");
     divEl.append(innerDivEl);
-    var titleEl = $("<h4>").attr("class","my-0 fw-normal");
+    var titleEl = $("<h4>").attr("class","my-0 fw-normal").attr("id","title-"+1);
     titleEl.text(city +" " + currentDate);
     innerDivEl.append(titleEl);
     var listEl = $("<ul>").attr("class","list-unstyled mt-3 mb-4");
     innerDivEl.append(listEl);
-    var li1El = $("<li>").text("Temperature: " + data.main.temp +"F");
+    
+    var li1El = $("<li>").attr("id","temp-"+1).text("Temperature: " + data.main.temp +"F");
     listEl.append(li1El);
-    var li2El = $("<li>").text("Humidity: " + data.main.humidity + "%");
+    var li2El = $("<li>").attr("id","humidity-"+1).text("Humidity: " + data.main.humidity + "%");
     listEl.append(li2El);
-    var li3El = $("<li>").text("Wind: " + data.wind.speed + "mph");
+    var li3El = $("<li>").attr("id","wind-"+1).text("Wind: " + data.wind.speed + "mph");
     listEl.append(li3El);
 
-    setWeatherDataToLocalStorage(data)
-   
-    
+    setWeatherDataToLocalStorage(data);
 }
 
+
 function fiveDayDisplay(data) {
-  var city = $("#user-input").val().toLowerCase().trim();
+
+  var city_value = $("#user-input").val().toLowerCase().trim();
   var cardsDivEl = $("#cards-div");
   cardsDivEl.text("");
   saveDatafiveDay = [];
+  var element_id_index = 2
   for (var i=1; i<data.list.length; i+=8) {
     var dateEl = data.list[i].dt_txt.split(" ")[0];
     var tempEl = data.list[i].main.temp;
@@ -79,81 +81,80 @@ function fiveDayDisplay(data) {
     cardsDivEl.append(divEl);
     var innerDivEl = $("<div>").attr("class","card-body");
     divEl.append(innerDivEl);
-    var titleEl = $("<h4>").attr("class","my-0 fw-normal");
-    titleEl.text(city + " " + dateEl);
+    var titleEl = $("<h4>").attr("class","my-0 fw-normal").attr("id","title-"+(element_id_index));
+    titleEl.text(city_value + " " + dateEl);
     innerDivEl.append(titleEl);
     var listEl = $("<ul>").attr("class","list-unstyled mt-3 mb-4");
     innerDivEl.append(listEl);
-    var li1El = $("<li>").text("Temperature: " + tempEl +"F");
+    var li1El = $("<li>").attr("id","temp-"+(element_id_index)).text("Temperature: " + tempEl +"F");
     listEl.append(li1El);
-    var li2El = $("<li>").text("Humidity: " + humidityEl +"%");
+    var li2El = $("<li>").attr("id","humidity-"+(element_id_index)).text("Humidity: " + humidityEl +"%");
     listEl.append(li2El);
-    var li3El = $("<li>").text("Wind: " + windEl + "mph");
+    var li3El = $("<li>").attr("id","wind-"+(element_id_index++)).text("Wind: " + windEl + "mph");
     listEl.append(li3El);
 
     var fiveDayObj = {
-      City: city,
-      Date: dateEl,
-      Temperature: tempEl,
-      Humidity: humidityEl,
-      Wind: windEl,
+      city: city_value,
+      date: dateEl,
+      temperature: tempEl,
+      humidity: humidityEl,
+      wind: windEl,
     }
     saveDatafiveDay.push(fiveDayObj);
   }
-  setFiveDaysToStorage(city,saveDatafiveDay);
-  
-
+    setFiveDaysToStorage(city_value,saveDatafiveDay);
 }
+
+
+function setWeatherDataToLocalStorage(data) {
+    var city_value = $("#user-input").val().toLowerCase().trim();
+    var CurrentDayweatherObj = [
+      {
+        city: city_value,
+        date: currentDate,
+        temperature: data.main.temp,
+        humidity: data.main.humidity,
+        wind: data.wind.speed
+      }]
+    var savetoLocalStorage1 = localStorage.setItem(city_value,JSON.stringify(CurrentDayweatherObj));
+}
+
 
 
 function setFiveDaysToStorage(city,arrayOfFiveDays) {
-  var cityWeatherDataArray = JSON.parse(localStorage.getItem(city));
-  // console.log(cityWeatherDataArray);
-  // console.log(arrayOfFiveDays)
-  cityWeatherDataArray = cityWeatherDataArray.concat(arrayOfFiveDays); 
-  localStorage.setItem(city,JSON.stringify(cityWeatherDataArray));
-  
-  var sixDaysWeather =(JSON.parse(localStorage.getItem(city)));
-  console.log(sixDaysWeather);
-  displaySixDaysWeather(sixDaysWeather);
-  
+
+    var cityWeatherDataArray = JSON.parse(localStorage.getItem(city));
+    cityWeatherDataArray = cityWeatherDataArray.concat(arrayOfFiveDays);
+    localStorage.setItem(city,JSON.stringify(cityWeatherDataArray));
+    displaySixDaysWeather(cityWeatherDataArray);
 }
 
-function setWeatherDataToLocalStorage(data) {
-  console.log("thi set current date is called")
-    var city = $("#user-input").val().toLowerCase().trim();
-    var CurrentDayweatherObj = [
-      {
-        city:city,
-        Date: currentDate,
-        Temperature: data.main.temp,
-        Humidity: data.main.humidity,
-        Wind: data.wind.speed
-      }]
-    // console.log(CurrentDayweatherObj)
-    var savetoLocalStorage1 = localStorage.setItem(city,JSON.stringify(CurrentDayweatherObj));
-    // console.log(savetoLocalStorage1);
-}
 
-// forloop the sixdaysweather array, for loop , building the same structure but with the new location of the city,wind,humidity(dote notation)
 function displaySixDaysWeather(sixDaysWeather) {
   
-  var prevBtnLocation = $("#prev-btn-location");
-  for(var i =0; i <sixDaysWeather.length; i++){
+    var searchHistoryContainerEl = $("#searchHistoryContainer");
+    var btnHistoryCity = $("<button>").text(sixDaysWeather[0].city);
+    var cityHistory = sixDaysWeather[0].city;
+    searchHistoryContainerEl.append(btnHistoryCity);
     
-    var btnHistoryCity = $("<button>").text(sixDaysWeather[i].city);
-    prevBtnLocation.append(btnHistoryCity);
     btnHistoryCity.on("click", function(event){
       event.preventDefault();
-
-      
-      
-
-      
-      
-
+      displayResultsFromDataCity(event.target.innerText);
     })
-  }
-}
+    };
+
+  
+  function displayResultsFromDataCity(city){
+
+    var sixDaysWeather =(JSON.parse(localStorage.getItem(city)));
+    for (var i =0; sixDaysWeather.length > i; i++){
+      var titleEl = $("#title-"+(i+1)).text(sixDaysWeather[i].city + " " + sixDaysWeather[i].date);
+      var li1El = $("#temp-"+(i+1)).text("Temperature: " + sixDaysWeather[i].temperature +"F");
+      var li2El = $("#humidity-"+(i+1)).text(sixDaysWeather[i].humidity + "%");
+      var li3El = $("#wind-"+(i+1)).text(sixDaysWeather[i].wind + "mph");
+  }};
+
+ 
+  
 
 
